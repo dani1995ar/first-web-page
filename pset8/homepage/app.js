add9e3c00fc6e518c06abd54b15ff081233f64f3
Daniel <dani1995ar@hotmail.com>
2021-07-09 01:05:46 -0500

    finished navbar in index, added navbar in questionary, and added a few words to the autocorrector dict

--- pset8/homepage/app.js
+++ pset8/homepage/app.js
@@ -1,1 +1,23 @@
-l
+const   navSlide = ()=>{
+    const burger = document.querySelector('.menu-burger');
+    const  nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', ()=>{
+        // This toggles nav from out of the frame
+        nav.classList.toggle('nav-active');
+
+        // This bit here animates each one of the anchors
+        navLinks.forEach((links, index) => {
+            if (links.style.animation) {
+                links.style.animation = '';
+            }
+            else {
+                links.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.8}s`;
+            }
+        });
+        // Burger to cross animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
