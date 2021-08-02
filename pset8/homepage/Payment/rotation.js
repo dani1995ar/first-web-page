48f67261588cb76cc1aa302205f5cafe37a444c2
Daniel <dani1995ar@hotmail.com>
2021-08-02 17:38:39 -0500

    fixed issue with the CSS credit card flipping when clicking on the inputs and selects. Completely finished Payments page

--- pset8/homepage/Payment/rotation.js
+++ pset8/homepage/Payment/rotation.js
@@ -1,14 +1,21 @@
-const ccCardBack = document.querySelector('.cc-card-back');
-const ccCardFront = document.querySelector('.cc-card-front');
+const ccCardBack = document.querySelector(".cc-card-back");
+const ccCardFront = document.querySelector(".cc-card-front");
 
-var spinFront = ccCardFront.addEventListener("click", function () {
-    ccCardBack.setAttribute("style", "transform: rotateY(360deg);");
-    ccCardFront.setAttribute("style", "transform: rotateY(180deg);");
+var spinFront = ccCardFront.addEventListener("click", e => {
+    
+    if (e.target.tagName !==  "INPUT" && e.target.tagName !== "SELECT") {
+        ccCardBack.setAttribute("style", "transform: rotateY(360deg);");
+        ccCardFront.setAttribute("style", "transform: rotateY(180deg);");
+        console.log(e.target.tagName)
+    }
+
+
 });
 
-var spinBack = ccCardBack.addEventListener("click", function () {
-    ccCardBack.setAttribute("style", "transform: rotateY(180deg);");
-    ccCardFront.setAttribute("style", "transform: rotateY(0deg);");
+var spinBack = ccCardBack.addEventListener("click", e => {
+    if (e.target.tagName !== "INPUT") {
+        ccCardBack.setAttribute("style", "transform: rotateY(180deg);");
+        ccCardFront.setAttribute("style", "transform: rotateY(0deg);");
+        console.log(e)
+    }
 });
-
-document.querySelectorAll('input').removeEventListener("click", spinBack, true);
