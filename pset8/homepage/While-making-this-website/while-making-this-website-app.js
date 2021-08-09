7cf9b7251d587d100d6ffa24ae86ce817a18940d
Daniel <dani1995ar@hotmail.com>
2021-08-09 15:29:43 -0500

    finished with the 'while making this website...' page, after some work I got it working the way I wanted (or at least close enough), this should be the end of this project as I'd like to move on to more advanced things, it was a funny and very very hard project for me to do, but it definitely taught me a lot

---
+++ pset8/homepage/While-making-this-website/while-making-this-website-app.js
@@ -0,0 +1,9 @@
+var feltLike = ["Theres an artist in me...But he's on vacations...", "I have two chicken legs for hands...", "I would never finish it...", "I hadn't learn a thing for the past 7 months..."];
+var count = 3;
+setInterval(() => {
+  count--;
+  document.getElementById('felt-like-text').innerHTML = feltLike[count];
+  if(count == 0){
+    count = 3;
+  }
+}, 4999);
