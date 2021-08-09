82dcd5bcb32b45131d29c48bfdb7a90d87e9de03
Daniel <dani1995ar@hotmail.com>
2021-08-09 16:58:05 -0500

    small typo fix

--- pset8/homepage/While-making-this-website/while-making-this-website-app.js
+++ pset8/homepage/While-making-this-website/while-making-this-website-app.js
@@ -1,4 +1,4 @@
-var feltLike = ["Theres an artist in me...But he's on vacations...", "I have two chicken legs for hands...", "I would never finish it...", "I hadn't learn a thing for the past 7 months..."];
+var feltLike = ["There's an artist in me...But he's on vacations...", "I have two chicken legs for hands...", "I would never finish it...", "I hadn't learn a thing for the past 7 months..."];
 var count = 3;
 setInterval(() => {
   count--;
